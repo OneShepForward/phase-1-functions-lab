90cdd4e80758c583f2bd9e3bafa477b172342ead
Nicholas Shephard <shephardn@gmail.com>
2021-11-01 19:58:08 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(startBlock) {
+    return Math.sqrt((42-startBlock)**2);
+}
+
+function distanceFromHqInFeet(block) {
+    return ((distanceFromHqInBlocks(block))*264);
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.sqrt(((startBlock - endBlock)*264)**2);
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return ((distance - 400) * 0.02);
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
